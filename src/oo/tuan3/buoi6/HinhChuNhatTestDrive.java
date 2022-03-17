8362f3ff22f24e2ebb2988ea522fb4d27834b0f2
baolhg2002 <giabaolehoang1006@gmail.com>
2022-03-17 15:41:50 +0700

    update all

---
+++ src/oo/tuan3/buoi6/HinhChuNhatTestDrive.java
@@ -0,0 +1,10 @@
+package oo.tuan3.buoi6;
+
+public class HinhChuNhatTestDrive {
+    public static void main(String[] args) {
+        HinhChuNhat hinhChuNhat = new HinhChuNhat();
+        hinhChuNhat.setChieuDai(10);
+        hinhChuNhat.setChieuRong(5);
+        System.out.println(hinhChuNhat);
+    }
+}
